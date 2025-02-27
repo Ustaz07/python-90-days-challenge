3bdc11bebcbb95ec1bbdeb382b9c362c1eea7cb8
Ustaz07 <shamsutukor@gmail.com>
2025-02-27 08:40:56 +0100

    exercise revision 35

--- day_36p.py
+++ day_36p.py
@@ -1,7 +1,9 @@
-# Index Error: As the name says, it is error when accessing index that does not exist.
+# Index Error: As the name says, it is error when accessing index that does not exist in list, topple, set, string etc.
+# nOTE that always: Index = [length-1] for every given set because it start from the offset 0 at the beginning.
 
 names = ["Jenny", "Piya", "Payal"]
 
+print(names[5])   # This will give index error
 # print(names[3])   # This will give index error
 # print(names[-1])  # This will give index error
 
