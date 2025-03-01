64669f1dd5d55c2a8e1e311f6a3f74837784167f
Ustaz07 <shamsutukor@gmail.com>
2025-03-01 12:59:47 +0100

    exercise revision 38

--- day_38p.py
+++ day_38p.py
@@ -1,19 +1,19 @@
-# Exercise
+# # Exercise
 
-list1 = [1,1,1]
-list2 = [1,1,1]
-list3 = [1,1,1]
-final_list = [list1, list2, list3]
+# list1 = [1,1,1]
+# list2 = [1,1,1]
+# list3 = [1,1,1]
+# final_list = [list1, list2, list3]
 
-dimension = input("Please enter the dimension of location to hide money e.g. 33: ")
-# print(dimension)
-row = int(dimension[0])
-column = int(dimension[1])
+# dimension = input("Please enter the dimension of location to hide money e.g. 33: ")
+# # print(dimension)
+# row = int(dimension[0])
+# column = int(dimension[1])
 
-row_selected = final_list[row-1]
-row_selected[column-1] = "X"
+# row_selected = final_list[row-1]
+# row_selected[column-1] = "X"
 
-print(f"The location is: \n {list1} \n {list2} \n {list3}")
+# print(f"The location is: \n {list1} \n {list2} \n {list3}")
 
 
 
@@ -40,4 +40,22 @@ print(f"The location is: \n {list1} \n {list2} \n {list3}")
 
 
 #WELCOME BACK EXERCISEs
+row1 = [1,1,1]
+row2 = [1,1,1]
+row3 = [1,1,1]
+matrix = [row1, row2, row3]
+# print(f"{row1}\n{row2}\n{row3}")
+
+dimension = input("Enter dimension to hide your money, (e.g 32):- " )
+
+row = int(dimension[0])
+column = int(dimension[1])
+
+row_selected = matrix[row - 1]
+row_selected[column - 1] = 'X'
+
+print(matrix)
+
+print(f"{row1}\n{row2}\n{row3}")
+
 
