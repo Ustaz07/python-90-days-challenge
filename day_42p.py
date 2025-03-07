bcca4c1b2baa93bf81501dfc730ea86a356f132f
Ustaz07 <shamsutukor@gmail.com>
2025-03-07 16:49:50 +0100

    Ramadan day 7, 3/5/2025, for loop

--- day_42p.py
+++ day_42p.py
@@ -7,13 +7,75 @@ for name in names:
 
 for list1 in ["Gmj", "Sman", "Ibm", "Qsm", "Alj"]:
     print(list1)
-# numbers = [2, 3, 5, -2, 10]
 
-# squares = []
-# for i in numbers:
-#     square = i ** 2
-#     squares.append(square)
+list2 = "Jenny"
+for i in list2:
+    print(i)
 
-# print(squares)
+list5 = ['Jenny', 'Ram', 'Shyam']
+for i in list5:
+    print(i)
+    if i == 'Ram':
+        print('Hey its me')
+
+
+
+
+items = [2, 4, 8, 10]
+
+for i in items:
+    square = i ** 2
+    print(square)
+
+
+
+# numbers = [1, 3, 5, 7, 9]
+
+# squa = []
+# for x in numbers:
+#     squ = x ** 2
+#     squa.append(squ)
+#     print(squa)
+
+
+
+
+
+numbers = [1, 3, 5, 7, 9]
+
+squa = []
+for x in numbers:
+    squ = x ** 2
+    squa.append(squ)
+print(squa)
+print("The list of squa is : ", squa)
+print(f"The list of squa is: {squa}")
+
+
+
+
+#******************for loop use with tuple************** 
+
+numbers = (1, 5, 7)
+
+squa = ()
+for x in numbers:
+    squ = x ** 2
+    squa.append(squ)
+print(squa)
+print("The list of squa is : ", squa)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
