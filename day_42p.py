f45ce9d90fd8609f6f8f69dcc97908932a04062b
Ustaz07 <shamsutukor@gmail.com>
2025-03-07 12:18:38 +0100

    for loop

---
+++ day_42p.py
@@ -0,0 +1,19 @@
+#For loop : Is used for traversing a seqence. ******************
+# The seqence may be string, list, tuple etc
+
+names = ["Gmj", "Sman", "Ibm", "Qsm", "Alj"]
+for name in names:
+    print(names)
+
+for list1 in ["Gmj", "Sman", "Ibm", "Qsm", "Alj"]:
+    print(list1)
+# numbers = [2, 3, 5, -2, 10]
+
+# squares = []
+# for i in numbers:
+#     square = i ** 2
+#     squares.append(square)
+
+# print(squares)
+
+
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,11 @@
-superset
+
+# numbers = [2, 3, 5, -2, 10]
+
+# squares = []
+# for i in numbers:
+#     square = i ** 2
+#     squares.append(square)
+
+# print(squares)
+
+
