600cf59ea5d84ecd0e4720d39b716282a8ffadff
Ustaz07 <shamsutukor@gmail.com>
2025-03-30 01:17:03 +0100

    Ramadan 29th Sallah night

--- day_49p.py
+++ day_49p.py
@@ -1,13 +1,118 @@
+import random
+# # Password Generator
+# # Work on easy and hard level
+# # use random no's, for loop, range,  small abc, capital ABC, no's 0 to 9, symbols
+
+# print("Welcome to password Generator!: ")
+
+# letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", 
+# "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", 
+# "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", 
+# "U", "V", "W", "X", "Y", "Z",]
+
+# symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "=", 
+# "/", ":", "|", ";", "~", "'", "`",]
+
+# numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+# n_letters = int(input("How many letters would you like in your password?: "))
+
+# n_symbols = int(input("How many symbols would you like?: "))
+
+# n_numbers = int(input("How many no's would you like?: "))
+
+# # for i in range(0,)
+
+# print(n_letters)
+# print(n_symbols)
+# print(n_numbers)
+# print(type(n_letters))
+# print(type(n_symbols))
+# print(type(n_numbers))
+
+# password = ""
+# for i in range(0,n_letters):
+#     char = random.choice(letters)
+#     password = password + char
+# print(password)
+
+# for i in range(0,n_numbers):
+#     char = random.choice(numbers)
+#     password = password + char
+# print(password)
+
+# for i in range(0,n_symbols):
+#     char = random.choice(symbols)
+#     password = password + char
+# print(password)
+
+
+
+
+
 # Password Generator
 # Work on easy and hard level
+# use random no's, for loop, range,  small abc, capital ABC, no's 0 to 9, symbols
 
-print("Welcome to the password Generator!: ")
+print("Welcome to password Generator!: ")
 
-input("How many letters would you like in your password?: ")
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", 
+"o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", 
+"E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", 
+"U", "V", "W", "X", "Y", "Z",]
 
-input("How many symbols would you like?: ")
+symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "=", 
+"/", ":", "|", ";", "~", "'", "`",]
 
-input("How many no's would you like?: ")
+numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+n_letters = int(input("How many letters would you like in your password?: "))
+
+n_symbols = int(input("How many symbols would you like?: "))
+
+n_numbers = int(input("How many no's would you like?: "))
+
+# for i in range(0,)
+
+print(n_letters)
+print(n_symbols)
+print(n_numbers)
+print(type(n_letters))
+print(type(n_symbols))
+print(type(n_numbers))
+
+password = []                      # we take the password as list[] instead of string""
+char = []
+for i in range(0,n_letters):
+    
+    char = random.choice(letters)
+    password = password + char
+
+
+for i in range(0,n_numbers):
+    char = random.choice(numbers)
+    password = password + char
+
+
+for i in range(0,n_symbols):
+    char = random.choice(symbols)
+    password = password + char
+print(password)
+
+password = random.shuffle(password)
+print(password)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
