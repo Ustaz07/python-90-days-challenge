ad490e112387bdf1cb84a67d747ea6e0490a96d9
Ustaz07 <shamsutukor@gmail.com>
2025-03-31 14:37:42 +0100

    Indentation

---
+++ day_52p.py
@@ -0,0 +1,9 @@
+# Indentation: Indentation is very important in python, because of not using curley braces like C.
+# We leave 4 spaces to indent, equivalent to tab.
+# It is better to used space to indent than tab.
+
+for i in range(5):
+    print("Hi")
+    print("Jenny", i)
+    if i == 3:
+        print("Welcome")
