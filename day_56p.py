6c2ac55631db1b4a85585314317331d455435864
Ustaz07 <shamsutukor@gmail.com>
2025-04-15 09:18:25 +0100

    Reebog's world ((1))

---
+++ day_56p.py
@@ -0,0 +1,41 @@
+# Function: what is a function, why do we need function, benefits, 
+# how to define a function, types.
+
+# Function is a block of organize reusable code that is used to perform some 
+#       specific task when it is called.
+
+#****************************REEBOG'S WORLD!*************************
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+move()
+move()
+move()
+turn_left()
+move()
+move()
+move()
+turn_right()
+move()
+turn_right()
+move()
+move()
+move()
+turn_left()
+
+
+
+# Exercise.....................................................
+def turn_around():
+    turn_left()
+    turn_left()
+
+move()
+move()
+move()
+turn_around()
+move()
+move()
+move()
